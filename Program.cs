6cd99c39088cc15204a94ac83403025e078457cc
varadarajan <varadarajanm@gmail.com>
2025-08-20 00:35:29 +0530

    test

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.MapGet("/", () => "Hello World from .NET 8 on Beanstalk!");
 var summaries = new[]
 {
